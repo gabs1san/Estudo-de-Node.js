c9c74b976c4d87c19178b142a2bdf6246c624601
gabs1san <sanbiel398@gmail.com>
2025-09-03 19:56:18 -0300

    Estudo Sintaxe

---
+++ app.js
@@ -0,0 +1,6 @@
+console.log("Hello World!"); //imprimir na tela 
+console.log('Hello World'); //Tudo que estiver entre "" '' `` seria uma string
+console.log(`Hello World`);
+
+console.log(12346); //Number
+console.log(1.2); 
