d00600f689a4a73163da33897169863b207c5e2f
gabs1san <sanbiel398@gmail.com>
2025-09-04 19:24:16 -0300

    sintxe básica

--- app.js
+++ app.js
@@ -7,3 +7,20 @@ console.log(1.2);
 
 console.log("Olá, estou aprendendo JavaScript e dentro desse repositório você irá encontrar a sintaxe básica");
 
+//-------Variáveis------//
+
+let nome1 = 'Gabriel'; // string
+let Nome; // variável undefined, declarada mas sem valor
+
+console.log(nome, 'nasceu em 2003');
+console.log('Em 2021', nome, 'iniciou a primeira faculdade de Sistemas de informação');
+console.log(nome +  'se formou em 2024 e iniciou a segunda faculdade em 2025'); // concatenação.
+console.log(nome + 'se forma em ADS no ano de 2027'); // concatenação.
+
+// não pode começar com número
+let 1nome;
+// não pode conter espaço ou traço;
+let nome cliente-cliente;
+//utilize camelCase
+let nomeCompleto = 'Gabriel Gregorio';
+//é utilizado Case-Sensitive
